de50db9e4fe326f47c0608b18a9ab28f0ff29292
April Domingo <aprildomingo3@gmail.com>
2018-12-09 23:04:48 -0500

    Commit 2 Tut 3

---
+++ Assets/Script/ConnectedWayPoint.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Code
+{
+
+    public class ConnectedWayPoint : Waypoint
+    {
+
+        [SerializeField]
+        protected float _connectivityRadius = 50f;
+
+        List<ConnectedWayPoint> _connections;   
+
+        public void Start()
+        {
+            GameObject[] allWaypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+
+            _connections = new List<ConnectedWayPoint>();
+
+            for(int i = 0; i < allWaypoints.Length; i++)
+            {
+                ConnectedWayPoint nextWaypoint = allWaypoints[i].GetComponent<ConnectedWayPoint>();
+
+                if(nextWaypoint != null)
+                {
+                    if(Vector3.Distance(this.transform.position, nextWaypoint.transform.position) <= _connectivityRadius && nextWaypoint != this)
+                    {
+                        _connections.Add(nextWaypoint);
+                    }
+
+                }
+
+            }
+
+        }
+
+        public override void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, debugDrawRadius);
+
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, _connectivityRadius);
+
+        }
+
+        public ConnectedWayPoint NextWaypoint(ConnectedWayPoint previousWaypoint)
+        {
+            if(_connections.Count == 0)
+            {
+
+                Debug.LogError("Insufficient waypoint count. ");
+                return null;
+
+            }
+            else if(_connections.Count == 1 && _connections.Contains(previousWaypoint))
+            {
+
+                return previousWaypoint;
+
+            }
+            else
+            {
+                ConnectedWayPoint nextWaypoint;
+                int nextIndex = 0;
+
+                do
+                {
+                    nextIndex = UnityEngine.Random.Range(0, _connections.Count);
+                    nextWaypoint = _connections[nextIndex];
+
+                } while (nextWaypoint == previousWaypoint);
+
+                return nextWaypoint;
+
+            }
+        } 
+    }
+}
---
+++ Assets/Script/NPCConnectedPatrol.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Code
+{
+    public class NPCConnectedPatrol : MonoBehaviour
+    {
+
+        [SerializeField]
+        bool _patrolWaiting;
+
+        [SerializeField]
+        float _totalWaitTime = 3f;
+
+        [SerializeField]
+        float _switchProbability = 0.2f;
+
+        NavMeshAgent _navMeshAgent;
+        ConnectedWayPoint _currentWaypoint;
+        ConnectedWayPoint _previousWaypoint;
+
+
+
+        bool _traveling;
+        bool _waiting;
+        bool _patrolForward;
+        float _waitTimer;
+        int _waypointsVisited;
+
+        public void Start()
+        {
+            _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+            if (_navMeshAgent == null)
+            {
+                Debug.LogError("The nav mesh agent component is not attached to " + gameObject.name);
+
+            }
+            else
+            {
+                if (_currentWaypoint == null)
+                {
+                    GameObject[] allWaypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+
+                    if (allWaypoints.Length > 0)
+                    {
+                        while (_currentWaypoint == null)
+                        {
+                            int random = UnityEngine.Random.Range(0, allWaypoints.Length);
+                            ConnectedWayPoint startingWaypoint = allWaypoints[random].GetComponent<ConnectedWayPoint>();
+
+                            if (startingWaypoint != null)
+                            {
+                                _currentWaypoint = startingWaypoint;
+
+                            }
+                        }
+                    }
+                }
+                    else
+                    {
+                        Debug.LogError("Failed to find any waypoints for use in the scene. ");
+                    }
+
+                SetDestination();
+
+            }
+
+        }
+
+        void FixedUpdate()
+        {
+            if (Input.GetKey("escape"))
+                Application.Quit();
+        }
+
+        public void Update()
+        {
+
+            if (_traveling && _navMeshAgent.remainingDistance <= 1.0f)
+            {
+                _traveling = false;
+                _waypointsVisited++;
+
+                if (_patrolWaiting)
+                {
+                    _waiting = true;
+                    _waitTimer = 0f;
+                }
+                else
+                {
+                    SetDestination();
+                }
+
+            }
+
+            if (_waiting)
+            {
+                _waitTimer += Time.deltaTime;
+                if (_waitTimer >= _totalWaitTime)
+                {
+                    _waiting = false;
+
+                    SetDestination();
+
+                }
+            }
+        }
+
+
+
+        private void SetDestination()
+        {
+            if (_waypointsVisited > 0)
+            {
+                ConnectedWayPoint nextWaypoint = _currentWaypoint.NextWaypoint(_previousWaypoint);
+                _previousWaypoint = _currentWaypoint;
+                _currentWaypoint = nextWaypoint;
+
+            }
+
+            Vector3 targetVector = _currentWaypoint.transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+            _traveling = true;
+
+
+        }
+    }
+}
